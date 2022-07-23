3fb8c7a3bb90d42f737bf14396012df8e0bbaae1
nadeera09512 <nudayanga123456@gmail.com>
2022-07-23 14:41:04 +0530

    added a new println

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -23,7 +23,7 @@ public class StudentController {
 	@GetMapping("/getAllStudents")
 	@ResponseBody
 	public List<Student> getAllStudents(){
-		System.out.println("Fetching all Students now");
+		System.out.println("going to get all students");
 		return repo.findAllStudents();
 		
 	}
